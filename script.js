62fe33b963e6c4c640df5ee31b12952e57f9e4fe
genudwalesca <141366232+genudwalesca@users.noreply.github.com>
2024-12-03 09:15:08 -0300

    ok

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementByld('formulario').addEventListener('submit',function(event){
+document.getElementById('formulario').addEventListener('submit',function(event){
     event.preventDefault();
 
     //Obter as notas inseridas pelo usuário
@@ -10,7 +10,7 @@ document.getElementByld('formulario').addEventListener('submit',function(event){
     let media =( nota1 + nota2 + nota3) /3;
 
     //Exibir a mensagem de resultado
-    let resultadoDiv = document.getElementByld('resultado')
+    let resultadoDiv = document.getElementById('resultado')
     if (media>=6.0) {
         resultadoDiv.innerHTML = `<p class="aprovado">Aprovado! Sua média foi;${media.toFixed(2)}</p>`; 
     } else{resultadoDiv.innerHTML = `<p class="recuperação">Recuperação! Sua média foi;${media.toFixed(2)}</p>`;  
